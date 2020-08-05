894d3fde0f89c60c13e4cfc934c1abb014676990
tiki7732 <lsaidah@gmail.com>
2020-08-05 16:46:50 -0500

    added dominant method which is O(n log n) algorithm

--- big-octo.rb
+++ big-octo.rb
@@ -2,19 +2,46 @@ class Octopus
 
     attr_reader :fish_arr
     def initialize()
-        @fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+        #@fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
     end
 
-    def sluggish
+    def sluggish(array)
         fish = ''
-        @fish_arr.each_with_index do |fish1, ind1|
-            @fish_arr[ind1 +1..-1].each do |fish2|
+        array.each_with_index do |fish1, ind1|
+            array[ind1 +1..-1].each do |fish2|
                 fish = fish1.length > fish2.length ? fish1 : fish2
             end
         end
         fish
     end
+
+    def dominant(array)
+        return array if array.length <= 1
+        middle_ind = array.length/2
+        left = array.take(middle_ind)
+        right = array.drop(middle_ind)
+        merge(dominant(left), dominant(right))
+    end
+
+    def merge(left, right)
+        merged = []
+        prc = Proc.new { |num1, num2| num1 <=> num2}
+        until left.empty? || right.empty?
+            case prc.call(left.first.length, right.first.length)
+            when -1
+                merged.push(left.shift)
+            when 0 
+                merged.push(left.shift)
+            when 1
+                merged.push(right.shift)
+            end
+        end
+        merged.concat(left, right)
+    end
+
 end
 
 o = Octopus.new
-p o.sluggish
+fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+p o.sluggish(fish_arr)
+p o.dominant(fish_arr).last
