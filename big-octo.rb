d9a948b4a90ce818b7806db12a082747755621f0
tiki7732 <lsaidah@gmail.com>
2020-08-05 16:28:26 -0500

    added sluggish method which is a O(n^2) algorithm

--- big-octo.rb
+++ big-octo.rb
@@ -1,3 +1,20 @@
 class Octopus
 
+    attr_reader :fish_arr
+    def initialize()
+        @fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+    end
+
+    def sluggish
+        fish = ''
+        @fish_arr.each_with_index do |fish1, ind1|
+            @fish_arr[ind1 +1..-1].each do |fish2|
+                fish = fish1.length > fish2.length ? fish1 : fish2
+            end
+        end
+        fish
+    end
 end
+
+o = Octopus.new
+p o.sluggish
